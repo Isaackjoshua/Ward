df56a0c770e2c6edb86b8ace5b39058f3cf5858d
Isaack Joshua <isaackjoshua23@gmail.com>
2026-08-27 01:59:21 +0300

    Add Python scaffold: pyproject, bedside package, pytest smoke test, gitignore

---
+++ bedside/__init__.py
@@ -0,0 +1,6 @@
+"""Ward / bedside: drive a broken computer through fake peripherals.
+
+M0 is a skeleton. No Channel, QEMU, or screen-reading code lives here yet.
+"""
+
+__version__ = "0.0.0"
---
+++ tests/test_smoke.py
@@ -0,0 +1,5 @@
+import bedside
+
+
+def test_package_imports():
+    assert bedside.__version__ == "0.0.0"
